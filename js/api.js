fb87cddf95aba3021d71fd108a323519f669ddc2
Adam Burnstine <aburnstine02@gmail.com>
2025-04-13 16:23:52 -0400

    Added route to fetch user by username and password

--- js/api.js
+++ js/api.js
@@ -20,6 +20,26 @@ function fetchUsers() {
   
   // Expose the fetchUsers function for use in main.js
   window.fetchUsers = fetchUsers;
+
+/**
+ * Fetch a single user (typically the logged in user)
+ * @param {string} username
+ * @param {string} password
+ * @returns {Promise<any[]>} 
+ */
+function fetchUser(username, password) {
+  return fetch(`http://localhost:3000/users?username=${username}&password=${password}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error("Error fetching users:", error);
+      throw error;
+    })
+}
   
 
   /**
@@ -47,6 +67,23 @@ function createUser(newUser) {
     });
 }
 
+
+
+
 // Expose createUser function
 window.createUser = createUser;
 
+/** 
+ * Route to create a new game
+ * @param {Object} game - the details of the game
+ * @param {string} game.title - the title of the posted game
+ * @param {string} game.description - the description of the game
+ * @param {string} game.user_id the id of the user who created the game
+ */
+
+/**
+ * Fetches the "games" data and filters by sport and skill level
+ * @param {Array[string]} sports - the sports to search for
+ * @param {string} skill - the skill level to search for
+ * @return {Promise<Object>} the list of games found
+ */
