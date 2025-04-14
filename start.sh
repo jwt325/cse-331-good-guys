6220f9c1a693bbf70b570e8d14bb81547a8cc239
Jacob The Losen <jacob.thelosen1@gmail.com>
2025-04-14 12:14:18 -0400

    start script nohups

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-npx json-server --watch db.json --port 3000 &
-npx http-server -p 8000 &
+nohup npx json-server --watch db.json --port 3000 > json.log 2>&1 &
+nohup npx http-server -p 8000 > http.log 2>&1 &
