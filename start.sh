8322adbfde7342228c72303cd6617d0526b80c33
Jacob The Losen <jacob.thelosen1@gmail.com>
2025-04-14 12:58:22 -0400

    startup commands

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-nohup npx json-server --watch db.json --port 3001 > json.log 2>&1 &
-nohup npx http-server -p 8000 > http.log 2>&1 &
+nohup npx json-server --watch db.json --port 3000 --host 0.0.0.0 > json.log 2>&1 &
+nohup npx http-server -a 0.0.0.0 -p 8000 > http.log 2>&1 &
